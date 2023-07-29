969b9e4aacf836ca284a6a7553d6b2788eba0490
sid-tmbill <sid@tmbill.com>
2023-07-29 19:17:14 +0530

    chore: first

---
+++ test/keys.template.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+// Set a sender id and server key and save this under test/keys.js
+module.exports = {
+  SENDER_ID: "Your sender id here",
+  SERVER_KEY: "Your server key here",
+};
